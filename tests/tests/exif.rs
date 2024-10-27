981cdac45bdb6ce23667c6ae90c5e5c6c66ceb38
Vaiz <4908982+Vaiz@users.noreply.github.com>
2024-10-27 14:45:36 +0100

    test an image without exif

--- tests/tests/exif.rs
+++ tests/tests/exif.rs
@@ -1,18 +1,9 @@
-/*
 #[test]
-fn test_with_no_exif() -> anyhow::Result<()> {
-    let path = "test-data/sample-exif_1200x800.jpg";
-    let meta = video_info::extract_file_metadata(path)?;
-
-    let expected = video_info::MetaData {
-        width: 1200,
-        height: 800,
-        creation_date: None,
-    };
-    assert_eq!(expected, meta);
-    Ok(())
+fn test_no_exif() {
+    let path = "test-data/sample-no-exif_1200x800.jpg";
+    assert!(video_info::extract_file_metadata(path).is_err());
 }
-*/
+
 #[test]
 fn test_date() -> anyhow::Result<()> {
     let path = "test-data/sample-exif_1200x800_with_date.jpg";
