edb70cc24d794274da4e3ab90682c8701d819250
Vaiz <4908982+Vaiz@users.noreply.github.com>
2024-10-29 14:56:37 +0100

    replace lib description with README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-//! This library provides a straightforward API to extract essential metadata (such as dimensions
-//! and creation date) from media files. It operates efficiently with multiple file types and
-//! includes an optional fallback using the mediainfo tool for extended metadata extraction.
+#![doc = include_str!("../README.md")]
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
