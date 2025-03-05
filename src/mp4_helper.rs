9a9590596a19d4d279b89833431b3d8624b71bd0
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-03-05 19:38:50 +0100

    replace mp4 with re_mp4

--- src/mp4_helper.rs
+++ src/mp4_helper.rs
@@ -1,7 +1,6 @@
 use crate::Result;
 use crate::{Error, MetaData};
-use mp4::Mp4Track;
-use std::collections::HashMap;
+use mp4::Track;
 use std::io;
 use std::time::SystemTime;
 
@@ -10,11 +9,11 @@ use std::time::SystemTime;
 /// This function utilizes the `mp4` crate to obtain metadata, including dimensions and creation
 /// date, from an MP4 container.
 pub fn extract_mp4_metadata<R: io::Read + io::Seek>(io: R, file_size: u64) -> Result<MetaData> {
-    let mp4 = mp4::Mp4Reader::read_header(io, file_size)?;
+    let mp4 = mp4::Mp4::read(io, file_size)?;
 
     let video_track = find_video_track(mp4.tracks());
     let (width, height) = if let Some(track) = video_track {
-        (track.width() as u64, track.height() as u64)
+        (track.width as u64, track.height as u64)
     } else {
         (0, 0)
     };
@@ -32,7 +31,7 @@ pub(crate) fn extract_mp4_creation_date<R: io::Read + io::Seek>(
     io: R,
     file_size: u64,
 ) -> Result<SystemTime> {
-    let mp4 = mp4::Mp4Reader::read_header(io, file_size)?;
+    let mp4 = mp4::Mp4::read(io, file_size)?;
 
     convert_mp4_time_to_system_time(mp4.moov.mvhd.creation_time).ok_or(Error::CreationDateNotFound)
 }
@@ -49,10 +48,11 @@ fn convert_mp4_time_to_system_time(mp4_time: u64) -> Option<SystemTime> {
     Some(utc.into())
 }
 
-fn find_video_track(tracks: &HashMap<u32, Mp4Track>) -> Option<&Mp4Track> {
-    const NOT_VIDEO: mp4::TrackType = mp4::TrackType::Subtitle;
+fn find_video_track<'a>(tracks: impl IntoIterator<Item = (&'a u32, &'a Track)>) -> Option<&'a Track> {
+    use mp4::TrackKind;
+    const NOT_VIDEO: TrackKind = TrackKind::Subtitle;
     tracks
-        .iter()
-        .find(|(_, track)| mp4::TrackType::Video == track.track_type().unwrap_or(NOT_VIDEO))
+        .into_iter()
+        .find(|(_, track)| TrackKind::Video == track.kind.unwrap_or(NOT_VIDEO))
         .map(|(_, track)| track)
 }
